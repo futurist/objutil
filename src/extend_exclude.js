fa90b8f038bf406be466d8af60c0b0055e7ed5be
1111hui <26634873@qq.com>
2016-06-25 11:13:34 +0800

    ADD: ownProperty check

--- src/extend_exclude.js
+++ src/extend_exclude.js
@@ -1,46 +1,48 @@
 
 var type = {}.toString
+var own = {}.hasOwnProperty
 var OBJECT = type.call({})
 
-function _deepIt(a, b, callback) {
-    if (a == null || b == null) {
-        return a;
+function _deepIt (a, b, callback) {
+  if (a == null || b == null) {
+    return a
+  }
+  for ( var key in b) {
+    if (!own.call(b, key)) continue
+    if (type.call(b[key]) == OBJECT) {
+      if (type.call(a[key]) != OBJECT) {
+        callback(a, b, key)
+      } else {
+        a[key] = _deepIt(a[key], b[key], callback)
+      }
+    } else {
+      callback(a, b, key)
     }
-    for( var key in b ) {
-        if (type.call(b[key]) == OBJECT) {
-            if (type.call(a[key]) != OBJECT) {
-                callback(a, b, key)
-            } else {
-                a[key] = _deepIt(a[key], b[key], callback);
-            }
-        } else {
-            callback(a, b, key)
-        }
-    }
-    return a;
+  }
+  return a
 }
 
-function _extend(x,y ){
-    return _deepIt(x,y, function(a,b,key){
-        a[key] = b[key]
-    })
+function _extend (x, y) {
+  return _deepIt(x, y, function (a, b, key) {
+    a[key] = b[key]
+  })
 }
 
 /*Usage: _exlucde(obj, {x:{y:1, z:1} }, [null] ) will delete x.y,x.z on obj, or set to newVal if present */
 // _exclude( {a:1,b:{d:{ c:2} } }, { b:{d:{ c:1} } } )
-function _exclude( x,y, newVal ){
-    var args = arguments
-    return _deepIt(x,y, function(a,b,key){
-        if( typeof b[key]!=='object' && b[key] ){
-            args.length==3 ? a[key]=newVal : delete a[key]
-        } else {
-            a[key] = b[key]
-        }
-    })
+function _exclude (x, y, newVal) {
+  var args = arguments
+  return _deepIt(x, y, function (a, b, key) {
+    if (typeof b[key] !== 'object' && b[key]) {
+      args.length == 3 ? a[key] = newVal : delete a[key]
+    } else {
+      a[key] = b[key]
+    }
+  })
 }
 
 export default {
-    _deepIt: _deepIt,
-    _extend: _extend,
-    _exclude: _exclude
+  _deepIt: _deepIt,
+  _extend: _extend,
+  _exclude: _exclude
 }

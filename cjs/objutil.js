f476c13d26140abb528108560f90667067a160f0
1111hui <26634873@qq.com>
2016-07-09 09:49:38 +0800

    CHANGE: name using objutil

--- cjs/objutil.js
+++ cjs/objutil.js
@@ -1,3 +1,4 @@
+'use strict';
 
 // better type check
 var is = function (t, v) { return {}.toString.call(v).slice(8, -1) === t }
@@ -11,20 +12,20 @@ function isPrimitive (val) {
   return !/obj|func/.test(typeof val) || !val
 }
 
-function _deepIt (a, b, callback, path) {
+function deepIt (a, b, callback, path) {
   path = path || []
   if (isPrimitive(b)) return a
   for ( var key in b) {
     if (!own(b, key)) continue
     callback(a, b, key, path, key in a)
     if (isIterable(b[key]) && isIterable(a[key])) {
-      _deepIt(a[key], b[key], callback, path.concat(key))
+      deepIt(a[key], b[key], callback, path.concat(key))
     }
   }
   return a
 }
 
-function _get(obj, p, errNotFound) {
+function get(obj, p, errNotFound) {
   var n = obj
   for(var i = 0, len = p.length; i < len; i++) {
     if(!isIterable(n) || !(p[i] in n))
@@ -34,10 +35,10 @@ function _get(obj, p, errNotFound) {
   return n
 }
 
-function _extend () {
+function extend () {
   var arg = arguments, last
   for(var i=arg.length; i--;) {
-    last = _deepIt(arg[i], last, function (a, b, key, path, inA) {
+    last = deepIt(arg[i], last, function (a, b, key, path, inA) {
       if(!inA || isPrimitive(b[key])) a[key] = b[key]
     })
   }
@@ -48,8 +49,8 @@ function _extend () {
  *  when isSet, will set value to a instead of delete
  */
 // _exclude( {a:1,b:{d:{ c:2} } }, { b:{d:{ c:1} } } )
-function _exclude (x, y, isSet) {
-  return _deepIt(x, y, function (a, b, key) {
+function exclude (x, y, isSet) {
+  return deepIt(x, y, function (a, b, key) {
     if (isPrimitive(b[key])) {
       isSet
         ? (key in a ? a[key] = b[key] : '')
@@ -58,43 +59,47 @@ function _exclude (x, y, isSet) {
   })
 }
 
-function _pick(obj, props) {
+function pick(obj, props) {
   var o={}
-  return _deepIt(o, props, function(a,b,key,path){
-    var c = _get(obj,path.concat(key))
+  return deepIt(o, props, function(a,b,key,path){
+    var c = get(obj,path.concat(key))
     if(!b[key]) return
     if(!isPrimitive(c)) a[key] = is('Array', c) ? [] : {}
     if(isPrimitive(b[key])) a[key] = c
   })
 }
 
-function _pick2(obj, props) {
+function pick2(obj, props) {
   props=props||{}
   var o={}
-  return _deepIt(o, obj, function(a,b,key,path){
-    var c = _get(props,path.concat(key))
+  return deepIt(o, obj, function(a,b,key,path){
+    var c = get(props,path.concat(key))
     if(c && isPrimitive(c)) return
     if(!isPrimitive(b[key])) a[key] = is('Array', b[key]) ? [] : {}
     else a[key]= b[key]
   })
 }
 
-function _default(obj, option) {
-  return _deepIt(obj, option, function(a,b,key){
+function defaults(obj, option) {
+  return deepIt(obj, option, function(a,b,key){
     if(!(key in a)) a[key]=b[key]
   })
 }
 
-export default {
-  _is: is,
-  _own: own,
-  _isIter: isIterable,
-  _isPrim: isPrimitive,
-  _get: _get,
-  _deepIt: _deepIt,
-  _extend: _extend,
-  _pick: _pick,
-  _pick2: _pick2,
-  _default: _default,
-  _exclude: _exclude
+var objutil = {
+  is: is,
+  own: own,
+  isIterable: isIterable,
+  isIter: isIterable,
+  isPrimitive: isPrimitive,
+  isPrim: isPrimitive,
+  get: get,
+  deepIt: deepIt,
+  extend: extend,
+  pick: pick,
+  pick2: pick2,
+  defaults: defaults,
+  exclude: exclude
 }
+
+module.exports = objutil;
--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var assert = require('assert')
 var expect = require('chai').expect
-var lib = require('../cjs/extend_exclude')
+var lib = require('../cjs/objutil.js')
 
 var a,b,c
 var newObj = function (obj){
@@ -40,8 +40,8 @@ var excludeList = {
 }
 
 describe('Test a/b with lib', function  () {
-	it('@ _extend b to a', function(){
-		expect( lib._extend(a,b) ).to.be.deep.equal(
+	it('@ extend b to a', function(){
+		expect( lib.extend(a,b) ).to.be.deep.equal(
 			{
 				"name": "James",
 				"age": 10,
@@ -58,8 +58,8 @@ describe('Test a/b with lib', function  () {
 		)
 	})
 
-	it('@ _extend with multiple args', function(){
-		expect( lib._extend(a,b,c) ).to.be.deep.equal(
+	it('@ extend with multiple args', function(){
+		expect( lib.extend(a,b,c) ).to.be.deep.equal(
 			{
 				"name": "James",
 				"age": 33,
@@ -76,15 +76,15 @@ describe('Test a/b with lib', function  () {
 		)
 	})
 
-	it('@ _exclude without newVal', function(){
-		var val = lib._exclude(a, excludeList)
+	it('@ exclude without newVal', function(){
+		var val = lib.exclude(a, excludeList)
 		expect( val ).is.deep.equal(
 			{"name":"James","prop":{"sn":1001,"order":[{item:'apple', number:10}, {item:'pear', number:23}]}}
 		)
 	})
 
-	it('@ _exclude with newVal', function(){
-		var val = lib._exclude(a, {
+	it('@ exclude with newVal', function(){
+		var val = lib.exclude(a, {
       hoho:234,
 	    age:null,
 	    prop:{ addr:null, lulu:true }
@@ -94,37 +94,37 @@ describe('Test a/b with lib', function  () {
 		)
 	})
 
-	it('@ _get with exist path', function(){
-		var val = lib._get(a, ['prop', 'order', 1, 'item'])
+	it('@ get with exist path', function(){
+		var val = lib.get(a, ['prop', 'order', 1, 'item'])
 		expect( val ).is.deep.equal('pear')
 	})
 
-	it('@ _get with non-exist path', function(){
-		var val = lib._get(a, 'prop/order/2/item'.split('/'))
+	it('@ get with non-exist path', function(){
+		var val = lib.get(a, 'prop/order/2/item'.split('/'))
 		expect( val ).to.be.undefined
 	})
 
-	it('@ _get with primitive path', function(){
-		var val = lib._get({prop:1}, 'prop/order/2/item'.split('/'))
+	it('@ get with primitive path', function(){
+		var val = lib.get({prop:1}, 'prop/order/2/item'.split('/'))
 		expect( val ).to.be.undefined
 	})
 
-	it('@ _get return Error on not found', function(){
-		var val = lib._get({prop:1}, 'prop/order/2/item'.split('/'), true)
+	it('@ get return Error on not found', function(){
+		var val = lib.get({prop:1}, 'prop/order/2/item'.split('/'), true)
 		expect( val ).to.be.an('error')
 		expect( val.message ).to.equal('NotFound')
 	})
 
 
-	it('@ _pick with exist obj path 1', function(){
-		var val = lib._pick({a:3, b:{c:2}}, {prop:{order:[0,1]}})
+	it('@ pick with exist obj path 1', function(){
+		var val = lib.pick({a:3, b:{c:2}}, {prop:{order:[0,1]}})
 		expect( val ).to.deep.equal(
       {}
     )
 	})
 
-	it('@ _pick with exist obj path 2', function(){
-		var val = lib._pick(a, {prop:{order:[0,1]}})
+	it('@ pick with exist obj path 2', function(){
+		var val = lib.pick(a, {prop:{order:[0,1]}})
 		expect( JSON.stringify(val) ).to.deep.equal(
       JSON.stringify({
         "prop":{"order":[null,{"item":"pear","number":23}]}
@@ -132,22 +132,22 @@ describe('Test a/b with lib', function  () {
     )
 	})
 
-	it('@ _pick2 with non-exist obj path 1', function(){
-		var val = lib._pick2({a:3, b:{c:2}}, {prop:{order:[0,1]}})
+	it('@ pick2 with non-exist obj path 1', function(){
+		var val = lib.pick2({a:3, b:{c:2}}, {prop:{order:[0,1]}})
 		expect( val ).to.deep.equal(
       {a:3, b:{c:2}}
     )
 	})
 
-	it('@ _pick2 with non-exist obj path 2', function(){
-		var val = lib._pick2(a, {prop:{order:[0,1]}})
+	it('@ pick2 with non-exist obj path 2', function(){
+		var val = lib.pick2(a, {prop:{order:[0,1]}})
 		expect( val ).to.deep.equal(
       {"name":"James","age":22,"prop":{"addr":"ABC","sn":1001,"order":[{"item":"apple","number":10}]}}
     )
 	})
 
-	it('@ _default with object', function(){
-		var val = lib._default({prop:{a:1}, b:2}, {prop:{a:10, order:[0,1]}, b:20, c:30})
+	it('@ defaults with object', function(){
+		var val = lib.defaults({prop:{a:1}, b:2}, {prop:{a:10, order:[0,1]}, b:20, c:30})
 		expect( val ).to.deep.equal(
       {"prop":{"a":1,"order":[0,1]},"b":2,"c":30}
     )
